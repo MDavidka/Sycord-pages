133e1d759aa8070f50f2ca636454bdb2f3e51c1e
Super User <root@sycord.site>
2026-09-23 17:55:19 +0530

    fix(admin): add missing Link import from next/link

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from "react"
 import Image from "next/image"
+import Link from "next/link"
 import { useSession, signOut } from "next-auth/react"
 import { useRouter } from "next/navigation"
 import {
